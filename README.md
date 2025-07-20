deb6b634b609d9042030f9b739423c57ac43a61c
Kent Brown <randall.k.brown.jr@gmail.com>
2025-07-19 23:53:08 -0500

    initial commit of v3 - rewrite as react-ts running on vite

---
+++ README.md
@@ -0,0 +1,18 @@
+# Personal Site README #
+
+Notes on running the site.
+
+## Installing the Site ##
+
+1. Clone the repository.
+2. Ensure that you have the correct versions of Node.js and NPM installed.
+3. Run `npm install`.
+
+## Running the Site ##
+
+1. Run `npm run dev`.
+
+## Deployment with AWS Amplify ##
+
+<forthcoming>
+
---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="frame">
+      <div className="flexy" />
+      <div className="app">
+        <h1>Randall Kent Brown Jr.</h1>
+        <div className="h-divider bg-primary" />
+        <div className="contents">
+          <p>Welcome to my site! I'm Kent Brown, a software developer from Wisconsin.</p>
+          <p>I've got five years of software development experience, drawn from time spent in numerous industries and many excellent teams.</p>
+          At RIT in Rochester, NY, I got my formal education in:
+          <ul>
+            <li>software engineering (B.S. 2021)</li>
+            <li>computer engineering (minor)</li>
+            <li>environmental studies (immersion)</li>
+          </ul>
+          <p>I love to play video games in my free time. I'm currently working my way through my third attempt at Elden Ring.</p>
+          Some of my other hobbies include:
+          <ul>
+            <li>Volleyball</li>
+            <li>Dungeons & Dragons</li>
+            <li>Reading <i>some recommendations:</i></li>
+              <ol>
+                <li>The 13 &amp; &frac12; Lives of Captain Bluebear - Walter Moers</li>
+                <li>The Anthropocene Reviewed - John Green</li>
+                <li>The Mythical Man-Month - Frederick P. Brooks Jr.</li>
+              </ol>
+          </ul>
+          There's not much else here yet, but thanks for visiting.
+        </div>
+      </div>
+      <div className="flexy" />
+    </div>
+  )
+}
+
+export default App
